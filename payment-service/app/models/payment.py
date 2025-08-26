7f5f2d25b0e2b7b9e7674dc2db965003d5cd8986
DerekMwirigi <derekmwirigi@gmail.com>
2025-08-26 19:24:57 +0300

    UPDATED: Added confirmation and validation models

--- payment-service/app/models/payment.py
+++ payment-service/app/models/payment.py
@@ -20,8 +20,27 @@ class Payment(BaseModel):
     updatedAt: datetime
 
 
-class MpesaCallback(BaseModel):
+class MpesaValidation(BaseModel):
     MerchantRequestID: str | None = None
     CheckoutRequestID: str | None = None
     ResultCode: int | None = None
     ResultDesc: str | None = None
+
+
+class MpesaConfirmation(BaseModel):
+    ResultCode: int | None = None
+    ResultDesc: str | None = None
+    TransactionType: str | None = None
+    TransID: str | None = None
+    TransTime: str | None = None
+    TransAmount: float | None = None
+    BusinessShortCode: str | None = None
+    BillRefNumber: str | None = None
+    InvoiceNumber: str | None = None
+    OrgAccountBalance: str | None = None
+    ThirdPartyTransID: str | None = None
+    MSISDN: str | None = None
+    FirstName: str | None = None
+    MiddleName: str | None = None
+    LastName: str | None = None
+    
